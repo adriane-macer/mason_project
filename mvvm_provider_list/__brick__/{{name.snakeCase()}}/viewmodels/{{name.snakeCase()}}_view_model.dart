28f94c5a81b67df1e28cf61d1ff2d4024c0e053e
Adrian Evangelista <adriane.macer@gmail.com>
2026-01-14 15:39:52 +0800

    api service brick

--- mvvm_provider_list/__brick__/{{name.snakeCase()}}/viewmodels/{{name.snakeCase()}}_view_model.dart
+++ mvvm_provider_list/__brick__/{{name.snakeCase()}}/viewmodels/{{name.snakeCase()}}_view_model.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import '../models/{{name.snakeCase()}}.dart';
 
 class {{name.pascalCase()}}ViewModel extends ChangeNotifier {
+final I{{name.pascalCase()}}Service _service;
+
+{{name.pascalCase()}}ViewModel(this._service);
+
 List<{{name.pascalCase()}}> _items = [];
 bool _isLoading = false;
+String? _errorMessage;
 
 List<{{name.pascalCase()}}> get items => _items;
 bool get isLoading => _isLoading;
+String? get errorMessage => _errorMessage;
 
-Future<void> fetch{{name.pascalCase()}}s() async {
+Future<void> loadData() async {
 _isLoading = true;
+_errorMessage = null;
 notifyListeners();
 
-// Simulating API call
-await Future.delayed(const Duration(seconds: 2));
-_items = List.generate(10, (i) => {{name.pascalCase()}}(id: i, title: '{{name.pascalCase()}} $i'));
-
+try {
+_items = await _service.fetchItems();
+} catch (e) {
+_errorMessage = e.toString();
+} finally {
 _isLoading = false;
 notifyListeners();
+}
 }
 }
